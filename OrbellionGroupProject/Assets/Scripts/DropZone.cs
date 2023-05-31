35f98ee7404ecbddc8888a297e1ebec6f301c43f
randypipkins <rpipkins@ewu.edu>
2023-05-31 14:57:44 -0700

    Finished draggable and dropzone scripts

--- OrbellionGroupProject/Assets/Scripts/DropZone.cs
+++ OrbellionGroupProject/Assets/Scripts/DropZone.cs
@@ -8,10 +8,30 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
     public Draggable.WhoseCard card = Draggable.WhoseCard.PlayerCard;
     public void OnPointerEnter(PointerEventData eventData){
         //Debug.Log("OnPointerEnter");
+        if(eventData.pointerDrag == null){
+            return;
+        }
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if(d != null){
+            if(card == d.card){
+                d.parentToReturnTo = this.transform;
+            }
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData){
         //Debug.Log("OnPointerExit");
+        if(eventData.pointerDrag == null){
+            return;
+        }
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if(d != null && d.placeholderParent == this.transform){
+            if(card == d.card){
+                d.placeholderParent = d.parentToReturnTo;
+            }
+        }
     }
 
     public void OnDrop(PointerEventData eventData){
