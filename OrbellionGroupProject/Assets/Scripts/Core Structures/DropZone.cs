fa4d037075424447aae37b991ccd3a421f31c2e8
randypipkins <rpipkins@ewu.edu>
2023-05-31 19:39:59 -0700

    changes

--- OrbellionGroupProject/Assets/Scripts/Core Structures/DropZone.cs
+++ OrbellionGroupProject/Assets/Scripts/Core Structures/DropZone.cs
@@ -5,43 +5,20 @@ using UnityEngine.EventSystems;
 
 public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    public Draggable.WhoseCard card = Draggable.WhoseCard.PlayerCard;
     public void OnPointerEnter(PointerEventData eventData){
-        //Debug.Log("OnPointerEnter");
-        if(eventData.pointerDrag == null){
-            return;
-        }
 
-        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
-        if(d != null){
-            if(card == d.card){
-                d.parentToReturnTo = this.transform;
-            }
-        }
-    }
-
-    public void OnPointerExit(PointerEventData eventData){
-        //Debug.Log("OnPointerExit");
-        if(eventData.pointerDrag == null){
-            return;
-        }
-
-        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
-        if(d != null && d.placeholderParent == this.transform){
-            if(card == d.card){
-                d.placeholderParent = d.parentToReturnTo;
-            }
-        }
     }
 
     public void OnDrop(PointerEventData eventData){
-        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
-
         Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
         if(d != null){
-            if(card == d.card){
-                d.parentToReturnTo = this.transform;
-            }
+            d.parentToReturnTo = this.transform;
         }
+
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+    }
+
+    public void OnPointerExit(PointerEventData eventData){
+
     }
 }
